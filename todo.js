6ea8a3cd284e576e4acf102e55c6ac21f8be0c4b
tprice04 <44441874+tprice04@users.noreply.github.com>
2020-12-01 13:35:20 -0600

    js

--- todo.js
+++ todo.js
@@ -3,6 +3,14 @@ window.onload= function(){
 	var newTask=document.getElementById ("new-task-create")
 	var addButton=document.getElementById("btn task") 
 
+var createNewTaskElement=function(taskString){
 
+	var listItem=document.createElement("ul");
+
+	var checkBox=document.createElement("input");
+
+	var label=document.createElement("label");
+
+	var editInput=document.createElement("input");
 
 }
