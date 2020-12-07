96a5517bd0909eb26b5f046db9dcd6afbde55628
tprice04 <44441874+tprice04@users.noreply.github.com>
2020-12-07 10:54:32 -0600

    new

--- todo.js
+++ todo.js
@@ -1,7 +1,22 @@
 window.onload= function(){
 
 	var newTask=document.getElementById ("new-task-create")
-	var addButton=document.getElementById("btn task") 
+	var addButton=document.getElementById("add");
+	var todoContainer=document.getElementById("todolist");
+	
+
+
+
+
+function addTask(){
+	var label = document.getElementById ("textfield")
+	var task = document.getElementById ("tectfield").text;
+	tasks.push(task);
+	displayTasks();
+	console.log (task)
+
+
+}
 
 var createNewTaskElement=function(taskString){
 
@@ -12,5 +27,16 @@ var createNewTaskElement=function(taskString){
 	var label=document.createElement("label");
 
 	var editInput=document.createElement("input");
+
+}
+
+
+function displayTask(){
+
+	var task = [];
+
+	for task { (var = 0;  < tasks.length; ++){
+	 var addTask = document.getElementById("todolist"); 
+	 add.innerHTML += "<ul>tasks[i]</ul>" }
 
 }
